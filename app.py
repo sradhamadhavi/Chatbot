4361f36f3d4393879f267117a8772ada0d8addc4
Hiteshwar Vadlamudi <hiteshv@Hiteshs-MacBook-Pro.local>
2017-03-28 21:47:57 -0400

    Updated the message

--- app.py
+++ app.py
@@ -38,11 +38,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    if message_text ==  "boxercraft"
-                        send_message(sender_id,"Key word Boxercraft Detected")
-                    else
-                        send_message(sender_id, "Thanks For your message, we will get back to you at the earliest")
+                    
+                    send_message(sender_id, "Thanks For your message, we will get back to you at the earliest")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
